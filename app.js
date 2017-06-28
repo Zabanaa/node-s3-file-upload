20dea69ecbd45a4d930bef73ab9e5e3f19781b7a
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-06-28 13:20:47 +0200

    upload file to aws and return url to the file

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app           = express()
 const port          = process.env.SANDBOX_PORT || 3000
 const uploadsDir    = path.join(__dirname, "uploads")
 
+
+// Config AWS
+AWS.config.loadFromPath("./creds.json")
+
+const myBucket      = new AWS.S3({params: { Bucket: process.env.S3_BUCKET }})
+
 app.set("view engine", "pug")
 
 // Execute file upload middleware
@@ -30,14 +36,24 @@ app.post("/upload", (req, res) => {
     }
 
     let myFile  = req.files.myfile
+    let data    = { Key: myFile.name, Body: myFile.data }
 
-    myFile.mv(path.join(uploadsDir, myFile.name), err => {
+    myBucket.putObject(data, (err, data) => {
 
         if (err) {
-            return res.status(500).send(err)
+            console.error("Error Uploading File: ", data)
+            return res.status(500).send("Error uploading file: " + data).end()
         }
 
-    return res.status(200).send("Thanks for uploading")
+        let urlParams = {Bucket: "zabanasandboxtest123", Key: myFile.name }
+
+        myBucket.getSignedUrl("getObject", urlParams, (err, url) => {
+
+            if (err) console.error("Couldn't retrieve url for this file")
+            return res
+                    .status(200)
+                    .send("Thanks for uploading, heres the link: " + url)
+        })
 
     })
 
@@ -60,12 +76,3 @@ app.listen(port, () => {
 
     console.log(`Server listening on port ${port}`)
 })
-
-// Config AWS
-// AWS.config.loadFromPath("./creds.json")
-
-// const s3        = new AWS.S3()
-
-// Create an s3 Bucket
-// const myBucket  = {Bucket: "myBucket"}
-// s3.createBucket(myBucket)
